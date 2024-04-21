18a0e365345723393ec49484cdfabd114f939b04
BlueIsADude <blueisdude@yahoo.com>
2024-04-20 21:36:12 -0500

    an attempt at adding boulderbark caves

--- src/main/java/net/bluethedude/woodnfungus/world/biome/ModOverworldRegion.java
+++ src/main/java/net/bluethedude/woodnfungus/world/biome/ModOverworldRegion.java
@@ -20,8 +20,7 @@ public class ModOverworldRegion extends Region {
     @Override
     public void addBiomes(Registry<Biome> registry, Consumer<Pair<MultiNoiseUtil.NoiseHypercube,
             RegistryKey<Biome>>> mapper) {
-        this.addModifiedVanillaOverworldBiomes(mapper, modifiedVanillaOverworldBuilder -> {
-            modifiedVanillaOverworldBuilder.replaceBiome(BiomeKeys.DRIPSTONE_CAVES, ModBiomes.BOULDERBARK_CAVES);
-        });
+        this.addModifiedVanillaOverworldBiomes(mapper, modifiedVanillaOverworldBuilder ->
+                modifiedVanillaOverworldBuilder.replaceBiome(BiomeKeys.DRIPSTONE_CAVES, ModBiomes.BOULDERBARK_CAVES));
     }
 }
